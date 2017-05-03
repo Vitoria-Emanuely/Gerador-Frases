7af63ae5ad4ccf5033a8deb5ea7fe8891d424ea2
Vitoria-Emanuely <vikaemanuely@gmail.com>
2017-05-03 11:05:46 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $titulo = ["Dom", "Nobre","Paladino"];
+    $qualidade = ["Abridor de galáxias", "Salvador da patria"];
+    $nome = "Jefferson";
+
+
+?>
+<html>
+
+    <head></head>
+
+    <body>
+        <h1>ola <?php echo "{titulo[rand(0,20]} $nome, {qualidade[rand(0,1)]}"; ?>0,
+        </h1>
+    </body>
+
+</html>
